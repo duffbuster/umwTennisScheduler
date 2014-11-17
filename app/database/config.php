57714e7838f1b2e3e04a1d2248089c66239e5f38
Colin Mackey <colin@fishbrains.com>
2014-11-17 17:59:09 -0500

    closing database after every use

--- app/database/config.php
+++ app/database/config.php
@@ -5,9 +5,10 @@
     $database = "mackeyde_angularScheduler";
 
     try {
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(PDO::ATTR_PERSISTENT => true));
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password/*, array(PDO::ATTR_PERSISTENT => true)*/);
         echo "Connected successfully";
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
+    $conn = null;
 ?>
