a29ae9096907f1a556efb5b6b1918787c2a18638
Colin Mackey <colin@fishbrains.com>
2014-11-17 10:41:18 -0500

    datepicker

--- app/app.js
+++ app/app.js
@@ -13,6 +13,40 @@ app.controller('AlertCtrl', function($scope) {
         $scope.closeAlert = function(index) {
             $scope.alerts.splice(index, 1);
         };
+}).controller('DatepickerCtrl', function($scope) {
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+    
+    $scope.clear = function() {
+        $scope.dt = null;
+    };
+    
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+    
+    $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.tottleMin();
+    
+    $scope.open = function($event){
+        $event.preventDefault();
+        $event.stopPropagation();
+        
+        $scope.opened = true;
+    };
+    
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+    
+    $scope.formats = ['dd-MMMM-yyyy', 'yyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
 });
 
 /*function databaseController($scope, $http) {
