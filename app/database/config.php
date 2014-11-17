74c80d849c8a15f7dac59f913444acdbe2ba4ec0
Colin Mackey <colin@fishbrains.com>
2014-11-17 17:55:13 -0500

    comment out try/catch

--- app/database/config.php
+++ app/database/config.php
@@ -4,10 +4,10 @@
     $hostname = "localhost";
     $database = "mackeyde_angularScheduler"
 
-    try {
+    //try {
         $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(PDO::ATTR_PERSISTENT => true));
         echo "Connected successfully";
-    } catch(PDOException $e) {
+   /* } catch(PDOException $e) {
         echo $e->getMessage();
-    }
+    }*/
 ?>
