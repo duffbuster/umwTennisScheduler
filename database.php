ad83afe96e3f90a3e7ab4260febff67eb59ff10e
Colin Mackey <colin@fishbrains.com>
2014-11-17 12:16:32 -0500

    database stuff

--- database.php
+++ database.php
@@ -2,9 +2,16 @@
 $username = "mackeyde_devteam";
 $password = "3devTeam!";
 $hostname = "localhost";
+$database = "mackeyde_angularScheduler"
 
 // connection
 $dbhandle = mysql_connect($hostname, $username, $password)
     or die("Unable to connect to MySQL");
 echo "Connected to MySQL<br>";
+
+$selected = mysql_select_db($database, $dbhandle)
+    or die("Could not select database");
+
+$result = mysql_query("SELECT * FROM events");
+
 ?>
