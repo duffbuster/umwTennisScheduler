1f1594baf5e6cbf4be4d043acfae0e85498b9419
Colin Mackey <colin@fishbrains.com>
2014-11-17 17:58:21 -0500

    fucking semi-colons

--- app/database/config.php
+++ app/database/config.php
@@ -2,12 +2,12 @@
     $username = "mackeyde_devteam";
     $password = "3devTeam!";
     $hostname = "localhost";
-    $database = "mackeyde_angularScheduler"
+    $database = "mackeyde_angularScheduler";
 
-    //try {
+    try {
         $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(PDO::ATTR_PERSISTENT => true));
         echo "Connected successfully";
-   /* } catch(PDOException $e) {
+    } catch(PDOException $e) {
         echo $e->getMessage();
-    }*/
+    }
 ?>
