9a33086f902f3352943c99c5ce094fe13a781fc1
Colin Mackey <colin@fishbrains.com>
2014-11-17 18:09:14 -0500

    more php bug fixes

--- app/database/getEvents.php
+++ app/database/getEvents.php
@@ -1,15 +1,21 @@
 <?php
     require 'config.php'; // database connection script
     
-    try {
-        $stmt = $conn->prepare("SELECT * FROM events");
-        $stmt->execute;
+    function getStmt() {
+        static $stmt = null;
+        if (is_null($stmt)) {
+            $stmt = $conn->prepare("SELECT * FROM events");
+        }
+        return $stmt;
+    }
 
+    $stmt = getStmt();
+    try {
+        $stmt->execute;
         // set array to associative
         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        
         echo json_encode($result);
     } catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
-    }
+    }     
 ?>
