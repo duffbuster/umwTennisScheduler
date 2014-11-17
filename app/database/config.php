eaeda8fbde11fe4392ed07a22f5ae34b6f9ffae8
Colin Mackey <colin@fishbrains.com>
2014-11-17 17:54:19 -0500

    php

--- app/database/config.php
+++ app/database/config.php
@@ -5,7 +5,7 @@
     $database = "mackeyde_angularScheduler"
 
     try {
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(PDO::ATTR_PERSISTENT => true));
         echo "Connected successfully";
     } catch(PDOException $e) {
         echo $e->getMessage();
