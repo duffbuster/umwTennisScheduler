54832218f8570e906571ec357517ece65de20079
Colin Mackey <colin@fishbrains.com>
2014-11-17 18:13:20 -0500

    merd

--- app/database/getEvents.php
+++ app/database/getEvents.php
@@ -1,21 +1,16 @@
 <?php
     require 'config.php'; // database connection script
     
-    function getStmt() {
-        static $stmt = null;
-        if (is_null($stmt)) {
-            $stmt = $conn->prepare("SELECT * FROM events");
-        }
-        return $stmt;
-    }
-
-    $stmt = getStmt();
     try {
+        global $stmt;
+        $stmt = $conn->prepare("SELECT * FROM events");
         $stmt->execute;
+
         // set array to associative
         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        
         echo json_encode($result);
     } catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
-    }     
+    }
 ?>
