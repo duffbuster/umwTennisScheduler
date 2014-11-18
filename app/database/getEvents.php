d3a297e370975dbfbfe3b428ec910ce2943323b5
Colin Mackey <colin@fishbrains.com>
2014-11-17 19:35:43 -0500

    Cleanup

--- app/database/getEvents.php
+++ app/database/getEvents.php
@@ -4,16 +4,4 @@
     $database->query('SELECT * FROM events');
     $rows = $database->resultSet();
     echo json_encode($rows);
-
-/*try {
-        $stmt = $conn->prepare("SELECT * FROM events");
-        $stmt->execute;
-
-        // set array to associative
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        
-        echo json_encode($result);
-    } catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }*/
 ?>
