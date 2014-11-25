74c888cee0fcefc67ec8a5bc23fceaf23041628d
Colin Mackey <colin@fishbrains.com>
2014-11-24 20:15:25 -0500

    login fix

--- app/database/login.php
+++ app/database/login.php
@@ -1,18 +1,18 @@
 <?php
     require 'config.php'; // database connection script
-    echo ($_POST['credentials']);
-    if (isset($_POST['credentials'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        
-        // Protect from SQL injection
-        $username = stripslashes($username);
-        $password = stripslashes($password);
-        $username = mysql_real_escape_string($username);
-        $password = mysql_real_escape_string($password);
-        
-        $database->query("SELECT * FROM users WHERE username='" + $username + "' AND password=password('" + $password + "')");
-        $row = $database->single();
-//        echo ("testing, testing 123");
-    }
+
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    $username = $request->username;
+    $password = $request->password;
+
+    // Protect from SQL injection
+    $username = stripslashes($username);
+    $password = stripslashes($password);
+    $username = mysql_real_escape_string($username);
+    $password = mysql_real_escape_string($password);
+
+    $database->query("SELECT * FROM users WHERE username='" + $username + "' AND password=password('" + $password + "')");
+    $row = $database->single();
+    echo json_encode($row);
 ?>
