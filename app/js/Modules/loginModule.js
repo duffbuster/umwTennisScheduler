984d8fa54f9899c5acaa9b78a7d969ef5d48d7d5
Colin Mackey <colin@fishbrains.com>
2014-11-24 20:03:39 -0500

    checking creds

--- app/js/Modules/loginModule.js
+++ app/js/Modules/loginModule.js
@@ -9,6 +9,7 @@ loginModule
     };
     
     $scope.login = function(credentials) {
+        console.log(credentials);
         AuthService.login(credentials).then(function(user) {
             $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
             $scope.setCurrentUser(user);
