09b3d5e9ee08ef6f611d381c8a58c3948590970a
Colin Mackey <colin@fishbrains.com>
2014-12-04 11:51:12 -0500

    This technically should work

--- app/js/Modules/viewEventsModule.js
+++ app/js/Modules/viewEventsModule.js
@@ -1,6 +1,6 @@
-var viewEventsModule = angular.module('viewEventsModule', ['ui.calendar']);
+var viewEventsModule = angular.module('viewEventsModule', []);
 
-viewEventsModule.controller('viewEventsCtrl', function($scope, Page) {
+viewEventsModule.controller('viewEventsCtrl', function($scope, Page, $http) {
     Page.setTitle('View Events')
     var date = new Date();
     var d = date.getDate();
@@ -8,74 +8,34 @@ viewEventsModule.controller('viewEventsCtrl', function($scope, Page) {
     var y = date.getFullYear();
 
     /* event source that contains custom events on the scope */
-    $scope.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1)},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false}
-    ];
-    /* event source that calls a function on every view switch */
-    $scope.eventsF = function (start, end, timezone, callback) {
-      var s = new Date(start).getTime() / 1000;
-      var e = new Date(end).getTime() / 1000;
-      var m = new Date(start).getMonth();
-      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
-      callback(events);
-    };
-
-    $scope.calEventsExt = {
-       color: '#f00',
-       textColor: 'yellow',
-       events: [ 
-          {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false}
-        ]
-    };
-
-    /* add and removes an event source of choice */
-    $scope.addRemoveEventSource = function(sources,source) {
-      var canAdd = 0;
-      angular.forEach(sources,function(value, key){
-        if(sources[key] === source){
-          sources.splice(key,1);
-          canAdd = 1;
+    $scope.eventSources = [
+        {
+            events: function() {
+                var data = $http({
+                    url: "/app/database/getEvents.php",
+                    method: "GET"
+                }).success(function(data) {
+                    var events = data;
+                    return events;
+                })
+            }
         }
-      });
-      if(canAdd === 0){
-        sources.push(source);
-      }
-    };
-
-    /* Change View */
-    $scope.changeView = function(view,calendar) {
-      uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
-    };
-    /* Change View */
-    $scope.renderCalender = function(calendar) {
-      if(uiCalendarConfig.calendars[calendar]){
-        uiCalendarConfig.calendars[calendar].fullCalendar('render');
-      }
-    };
+    ];
+    
+    // Will eventually need something to delete an event
     
     /* config object */
     $scope.uiConfig = {
       calendar:{
-        height: 450,
-        editable: true,
+        height: 500,
+        editable: false,
         header:{
           left: 'title',
           center: '',
           right: 'today prev,next'
         },
-        eventClick: $scope.alertOnEventClick,
-        eventDrop: $scope.alertOnDrop,
-        eventResize: $scope.alertOnResize,
+        eventClick: $scope.alertOnEventClick, //have this open a pre or something with the info (directive)
         eventRender: $scope.eventRender
       }
     };
-
-    /* event sources array*/
-    $scope.eventSources = [$scope.events, $scope.eventsF];
-    $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
 });
