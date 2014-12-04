c88cdaff5e55cc9a4f92886419fe4f63725b0c16
Colin <colin@fishbrains.com>
2014-12-03 21:56:53 -0500

    test events

--- app/js/Modules/viewEventsModule.js
+++ app/js/Modules/viewEventsModule.js
@@ -4,5 +4,20 @@ viewEventsModule.controller('viewEventsCtrl', function($scope, Page) {
     Page.setTitle('View Events');
     $scope.isLoginPage = false;
     
-    $scope.eventSources = [];
+    $scope.eventSources = [
+        {
+            events: [
+                {
+                    title: 'Event1',
+                    start: '2014-12-12'
+                },
+                {
+                    title: 'Event2',
+                    start: '2014-12-13'
+                }
+            ],
+            color: 'yellow',
+            textColor: 'black'
+        }
+    ];
 });
