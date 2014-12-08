61fe257716499af4f7edf0b3280f0823efb6782d
Colin Mackey <colin@fishbrains.com>
2014-12-07 22:57:26 -0500

    mafuckin' get requests

--- app/database/getEvents.php
+++ app/database/getEvents.php
@@ -1,7 +1,15 @@
 <?php
     require 'config.php'; // database connection script
-    
-    $database->query('SELECT * FROM events');
-    $rows = $database->resultSet();
-    echo json_encode($rows);
+
+    if ($_GET['e']) {
+        $database->query('SELECT * FROM events WHERE event_id=:id');
+        $database->bind(':id', $_GET['e']);
+        $res = $database->single();
+        echo json_encode($res);
+    }
+    else {
+        $database->query('SELECT * FROM events');
+        $rows = $database->resultSet();
+        echo json_encode($rows);
+    }  
 ?>
--- app/js/Modules/viewEventsModule.js
+++ app/js/Modules/viewEventsModule.js
@@ -15,7 +15,7 @@ viewEventsModule.controller('viewEventsCtrl', function($scope, Page, $http) {
                     url: "/app/database/getEvents.php",
                     method: "GET"
                 }).success(function(data) {
-                    /*var eventCount = data.length;
+                    var eventCount = data.length;
                     var source = [];
                     var jsonData = [];
                     for (var i = 0; i < eventCount; i++) {
@@ -29,9 +29,9 @@ viewEventsModule.controller('viewEventsCtrl', function($scope, Page, $http) {
                             method: "GET",
                             error: function() { alert('There was an error loading calendar data.'); }
                         });
-                    }*/
-                    var jsonData = data;
-//                    console.log(jsonData);
+                    }
+//                    var jsonData = data;
+                    console.log(jsonData);
                     return jsonData;
                 });
             }
